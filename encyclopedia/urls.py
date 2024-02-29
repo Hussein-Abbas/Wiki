b3d2f4c616d0cd1198d70c05f47abb075ac1bc3a
Hussein-Abbas <hussein.ihsan.abbas@gmail.com>
2024-02-29 06:05:13 +0300

    improve edit page path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki", views.index),
+    path("wiki/", views.index),
     path("wiki/search", views.search, name="search"),
     path("wiki/newpage", views.newpage, name="newpage"),
-    path("wiki/editpage", views.editpage, name="editpage"),
     path("wiki/randompage", views.randompage, name="randompage"),
+    path("wiki/<str:title>/editpage", views.editpage, name="editpage"),
     path("wiki/<str:title>", views.entry, name="entry"),
 ]
